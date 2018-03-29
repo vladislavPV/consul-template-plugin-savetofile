ce6b3c4c81b4c4db409d1d4c43e4869c052efed9
Vladislav <vladislavPV@users.noreply.github.com>
2018-03-29 10:18:41 -0400

    replace from certdump.go to support usernames

--- savetofile.go
+++ savetofile.go
@@ -4,87 +4,47 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/user"
 	"strconv"
 )
 
 func main() {
 	err := realMain()
-
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
-
 	os.Exit(0)
 }
 
 func realMain() error {
-	if len(os.Args) != 6 {
+	if len(os.Args) != 4 {
+		// Ensure the empty input case is handled correctly
 		return nil
 	}
 
-	// savetofile <mode> <filepath> <uid> <gid> <data>
-	mode := os.Args[1]
-	path := os.Args[2]
-	uid_arg := os.Args[3]
-	gid_arg := os.Args[4]
-	data := os.Args[5]
+	// certdump <filepath> <owner> <data>
+	path := os.Args[1]
+	owner := os.Args[2]
+	data := os.Args[3]
 
-	uid, err := strconv.Atoi(uid_arg)
+	err := ioutil.WriteFile(path, []byte(data), 0700)
 	if err != nil {
 		return err
 	}
-
-	gid, err := strconv.Atoi(gid_arg)
+	u, err := user.Lookup(owner)
 	if err != nil {
 		return err
 	}
-
-	switch mode {
-	case "append":
-		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0700)
-		if err != nil {
-			return err
-		}
-
-		defer f.Close()
-
-		if _, err = f.WriteString(data); err != nil {
-			return err
-		}
-	case "append-nl":
-		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0700)
-		if err != nil {
-			return err
-		}
-
-		defer f.Close()
-
-		if _, err = f.WriteString(data); err != nil {
-			return err
-		}
-
-		if _, err = f.WriteString("\n"); err != nil {
-			return err
-		}
-	case "create-nl":
-		err := ioutil.WriteFile(path, append([]byte(data), []byte("\n")...), 0700)
-		if err != nil {
-			return err
-		}
-	default: // "create"
-		err := ioutil.WriteFile(path, []byte(data), 0700)
-		if err != nil {
-			return err
-		}
+	uid, err := strconv.Atoi(u.Uid)
+	if err != nil {
+		return err
 	}
-
+	gid := os.Getgid()
+	err = os.Chmod(path, 0660)
+	if err != nil {
+		return err
+	}
 	err = os.Chown(path, uid, gid)
-	if err != nil {
-		return err
-	}
-
-	err = os.Chmod(path, 0640)
 	if err != nil {
 		return err
 	}
